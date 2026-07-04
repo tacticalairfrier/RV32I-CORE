7b1592dc7e6220340f6eca9120b581bdde22c3a9
Chinmay Tupkari <ctupkari5@gmail.com>
2026-07-04 15:04:54 +0530

    added the files for golden reference model

---
+++ ref/rv32i_ref.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<stdint.h>
+//this is the golden reference model or instruction set simulator for my risc-v core
+//this core is a part of a larger crypto core which i am going to create
