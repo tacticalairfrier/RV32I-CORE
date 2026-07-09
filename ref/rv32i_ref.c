210cb5c772134eed3c235a928840d85c096df255
Chinmay Tupkari <ctupkari5@gmail.com>
2026-07-09 15:21:28 +0530

    added most of the instruction in the isa golden model

--- ref/rv32i_ref.c
+++ ref/rv32i_ref.c
@@ -18,11 +18,14 @@ int main() {
     // uint8_t* inst_arr = (uint8_t*)malloc(80*sizeof(uint8_t));
     //using static arrays for testing
     uint8_t inst_arr[80]; // this can store like 4 instructions enough for verification
+    uint8_t mem_arr[80];
     //a simple for loop which does the part of reading every instruction over and over
     while (program_counter <= 80) {
         // //copying over the instruction into a temporary current instruction array because well i need to bitshift to hell
         inst_curr = (inst_arr[program_counter + 3] << 24 | inst_arr[program_counter + 2] << 16 | inst_arr[program_counter + 1] << 8 | inst_arr[program_counter]);
         //filtering the opcodes first
+        int32_t rs1, rs2;
+        uint32_t index;
         switch (inst_curr & 0x7f) {
         case 0x37: {
             //load upper intermediate lui
@@ -40,7 +43,7 @@ int main() {
         case 0x6f: {
             //jal jump and link
             registerfile[(inst_curr & 0xf80) >> 7] = program_counter += 4;
-            program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0xff000) << 11 | (inst_curr & 0x100000) << 2 | (inst_curr & 0x7fe00000) >> 9) >> 12) | ((inst_curr & 0x80000000) ? (0xfff00000) : (0x00000000));
+            program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0xff000) << 11 | (inst_curr & 0x100000) << 2 | (inst_curr & 0x7fe00000) >> 9) >> 11) | ((inst_curr & 0x80000000) ? (0xffe00000) : (0x00000000));
             break;
         }
         case 0x67: {
@@ -56,13 +59,13 @@ int main() {
         }
         case 0x63: {
             //branch cases
-            int32_t rs2 = registerfile[(inst_curr & 0x1f00000) >> 20];
-            int32_t rs1 = registerfile[(inst_curr & 0xf8000) >> 15];
+            rs2 = registerfile[(inst_curr & 0x1f00000) >> 20];
+            rs1 = registerfile[(inst_curr & 0xf8000) >> 15];
             switch ((inst_curr & 0x7000) >> 12) {
                 //beq -> branch if equal by offset
             case 0x0: {
                 if (rs1 == rs2) {
-                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 19) | ((inst_curr & 0x80000000) ? (0xffffe000) : (0x00000000));
                 }
                 else {
                     program_counter += 4;
@@ -72,7 +75,7 @@ int main() {
             case 0x1: {
                 //bne -> branch if not equal by offset
                 if (rs1 != rs2) {
-                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 19) | ((inst_curr & 0x80000000) ? (0xffffe000) : (0x00000000));
                 }
                 else {
                     program_counter += 4;
@@ -82,7 +85,7 @@ int main() {
             case 0x4: {
                 //blt ->branch if less than by offse
                 if (rs1 < rs2) {
-                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 19) | ((inst_curr & 0x80000000) ? (0xffffe000) : (0x00000000));
                 }
                 else {
                     program_counter += 4;
@@ -92,7 +95,7 @@ int main() {
             case 0x5: {
                 //bge -> branch if greater than or equal to 
                 if (rs1 >= rs2) {
-                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 19) | ((inst_curr & 0x80000000) ? (0xffffe000) : (0x00000000));
                 }
                 else {
                     program_counter += 4;
@@ -102,7 +105,7 @@ int main() {
             case 0x6: {
                 //bltu -> branch less than unsigned
                 if (registerfile[(inst_curr & 0xf8000) >> 15] < registerfile[(inst_curr & 0x1f00000) >> 20]) {
-                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 19) | ((inst_curr & 0x80000000) ? (0xffffe000) : (0x00000000));
                 }
                 else {
                     program_counter += 4;
@@ -111,8 +114,8 @@ int main() {
             }
             case 0x7: {
                 //bgeu -> branch greater than unsigned
-                if (registerfile[(inst_curr & 0xf8000) >> 15] > registerfile[(inst_curr & 0x1f00000) >> 20]) {
-                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+                if (registerfile[(inst_curr & 0xf8000) >> 15] >= registerfile[(inst_curr & 0x1f00000) >> 20]) {
+                    program_counter += ((inst_curr & 0x80000000 | (inst_curr & 0x7e000000) >> 1 | (inst_curr & 0x80) << 22 | (inst_curr & 0xf00) << 11) >> 19) | ((inst_curr & 0x80000000) ? (0xffffe000) : (0x00000000));
                 }
                 else {
                     program_counter += 4;
@@ -127,34 +130,87 @@ int main() {
             break;
         }
         case 0x03: {
-            //memory operations -> lb load byte, lh load halfword ,lw load word lbhu 
+            index = registerfile[(inst_curr & 0xf8000) >> 15] + (inst_curr >> 20) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+            //memory operations -> lb load byte, lh load halfword ,lw load word lbu -> load byte unsigned lhu -> load halfword unsigned
             switch ((inst_curr & 0x7000) >> 12) {
             case 0x00: {
-
+                //load byte and sign extend to 32 bites
+                registerfile[(inst_curr & 0xf80) >> 7] = (mem_arr[index]) | ((mem_arr[index] & 0x80) ? (0xffffff00) : (0x0));
+                program_counter += 4;
                 break;
             }
             case 0x01: {
+                //load halfword and sign extend by
+                registerfile[(inst_curr & 0xf80) >> 7] = (mem_arr[index + 1] << 8 | mem_arr[index]) | ((mem_arr[index + 1] & 0x80) ? (0xffff0000) : (0x0));
+                program_counter += 4;
                 break;
             }
             case 0x02: {
+                //load entire 32 bit word
+                registerfile[(inst_curr & 0xf80) >> 7] = mem_arr[index + 3] << 24 | mem_arr[index + 2] << 16 | mem_arr[index + 1] << 8 | mem_arr[index];
+                program_counter += 4;
                 break;
             }
             case 0x04: {
+                //load byte unsigned
+                registerfile[(inst_curr & 0xf80) >> 7] = (mem_arr[index]);
+                program_counter += 4;
                 break;
             }
             case 0x05: {
+                //load halfword unsigned
+                registerfile[(inst_curr & 0xf80) >> 7] = (mem_arr[index + 1] << 8 | mem_arr[index]);
+                program_counter += 4;
                 break;
             }
             default: {
+                program_counter += 4;
                 break;
             }
             }
             break;
         }
         case 0x23: {
+            //memory store operations sb-> store byte, stores byte  sh-> stores half word , sw -> stores entire 32 bit word
+            index = registerfile[(inst_curr & 0xf8000) >> 15] + ((inst_curr & 0xfe000000) >> 20 | (inst_curr & 0xf80) << 13) | ((inst_curr & 0x80000000) ? (0xfffff000) : (0x00000000));
+            switch ((inst_curr & 0x7000) >> 12) {
+            case 0x01: {
+                //sb -> stores byte
+                mem_arr[index] = registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff;
+                break;
+            }
+            case 0x02: {
+                //sh -> stores halfword
+                mem_arr[index + 1] = (registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff00) >> 8;
+                mem_arr[index] = registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff;
+                break;
+            }
+            case 0x04: {
+                //sw -> stores word
+                mem_arr[index + 3] = (registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff000000) >> 24;
+                mem_arr[index + 2] = (registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff0000) >> 16;
+                mem_arr[index + 1] = (registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff00) >> 8;
+                mem_arr[index] = registerfile[(inst_curr & 0x1f00000) >> 20] & 0xff;
+                break;
+            }
+            }
             break;
         }
         case 0x13: {
+            //immediate arithmetic instructions -> addi, slti, sltiu, xori, ori, andi, slli, srli, srai,
+            switch ((inst_curr & 0x7000) >> 12) {
+            case 0x00: {
+                //addi -> add intermediate i.e take 12 bit thing sign extend and add
+                break;
+            }
+            case:
+            case:
+            case:
+            case:
+            case:
+            case:
+            case:
+            }
             break;
         }
         case 0x33: {
