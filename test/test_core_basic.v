be095dc530c942b526c0aa72dcbbe70a7bee000a
Chinmay Tupkari <ctupkari5@gmail.com>
2026-09-05 01:07:54 +0530

    Refactor variable dumping in test_core_basic.v

--- test/test_core_basic.v
+++ test/test_core_basic.v
@@ -24,10 +24,69 @@ end
 initial begin
     $dumpfile("sim.vcd");
     //dumping of all the variables that exist here
-    $dumpvars(0, test_rv32i_basic);
-    for(i=0;i<32;i=i+1)begin
-        $dumpvars(0, RV32I_00.registerfile[i]);
-    end
+    $dumpvars(0,test_rv32i_basic);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[1]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[1]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[2]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[2]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[3]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[3]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[4]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[4]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[5]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[5]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[6]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[6]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[7]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[7]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[8]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[8]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[9]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[9]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[10]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[10]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[11]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[11]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[12]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[12]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[13]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[13]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[14]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[14]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[15]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[15]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[16]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[16]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[17]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[17]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[18]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[18]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[19]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[19]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[20]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[20]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[21]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[21]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[22]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[22]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[23]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[23]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[24]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[24]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[25]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[25]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[26]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[26]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[27]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[27]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[28]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[28]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[29]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[29]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[30]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[30]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs1[31]);
+    $dumpvars(0,RV32I_00.REG_0.registerfile_rs2[31]);
     for(i=0;i<40;i=i+1)begin
         $dumpvars(0, RV32I_00.MEM_0.dat_mem[i]);
     end
